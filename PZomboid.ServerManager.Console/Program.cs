0b12eca4d68e36f86c22d16a9117cde47743862c
pepenotti <antonio.franchinotti@gmail.com>
2023-04-15 00:30:30 -0300

    Add project files.

---
+++ PZomboid.ServerManager.Console/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PZomboid.ServerManager.Core;
+using System.Drawing;
+
+const string GREETING = "Welcome to Project Zomboid Server Management Console";
+const string GREETING_LINE = "====================================================";
+
+var linuxCommandService = new LinuxCommandService(
+                                "/opt/pzserver/", 
+                                "start-server.sh", 
+                                new[] { "-servername", "PZServer", "-config", "/home/pzuser/Zomboid/Server/PZServer.init" }, 
+                                "/home/pzuser/Zomboid/Server/");
+
+var exit = false;
+Console.WriteLine(GREETING);
+Console.WriteLine(GREETING_LINE, Color.Green);
+Console.WriteLine();
+
+while (!exit)
+{
+    ShowOptions();
+    var command = Console.ReadLine() ?? string.Empty;
+    await ExecuteCommandAsync(command);
+}
+
+void ShowOptions()
+{
+    Console.WriteLine("Please, select an option:");
+    Console.WriteLine("=========================", Color.Green);
+    Console.WriteLine("1 - Start server");
+    Console.WriteLine("2 - See console logs");
+    Console.WriteLine("3 - Execute commands");
+    Console.WriteLine("4 - Shutdown Server");
+    Console.WriteLine("e - Exit");
+    Console.WriteLine();
+}
+
+Task ExecuteCommandAsync(string command)
+{
+    switch (command.ToLowerInvariant())
+    {
+        case "1":
+            linuxCommandService.StartServer();
+            break;
+        case "2":
+            Console.WriteLine("Console output");
+            Console.WriteLine("==============");
+            Console.WriteLine();
+            Console.WriteLine(linuxCommandService.SeeConsoleLogs());
+            Console.WriteLine();
+            break;
+        case "3":
+            Console.WriteLine("What command are you going to run?");
+            var cmd = Console.ReadLine() ?? string.Empty;
+            linuxCommandService.ExecuteServerCommand(cmd);
+            break;
+        case "4":
+            linuxCommandService.ShutdownServerAsync();
+            break;
+        case "e":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine($"{command} option is not valid.");
+            break;
+    }
+
+    return Task.CompletedTask;
+}
---
+++ PZomboid.ServerManager.Core/CommandsService.cs
@@ -0,0 +1,80 @@
+﻿using CliWrap;
+using System.Text;
+
+namespace PZomboid.ServerManager.Core
+{
+    public class LinuxCommandService
+    {
+        private readonly string serverBashDirectory;
+        private readonly string serverBashFile;
+        private readonly string[] serverBashArgs;
+
+        private readonly string serverRunFilePath;
+
+        private const string SCREEN_CMD = "screen";
+        private const string SCREEN_SOCKNAME = "PZServer";
+        private const int SCREEN_EXEC_ARG_COMMAND_POSITION = 5;
+        private readonly string[] screenExecArgs = new[]
+        {
+            "screen", "-S", SCREEN_SOCKNAME, "-X", "stuff", "'{COMMAND}'", "`echo -ne '\\015'`"  //position 5 should be updated
+        };
+
+        private const string SERVER_COMMAND_SHUTDOWN = "quit";
+
+        public LinuxCommandService(
+            string serverBashDirectory,
+            string serverBashFile,
+            string[] serverBashArgs,
+            string serverRunFilePath)
+        {
+            this.serverBashDirectory = serverBashDirectory;
+            this.serverBashFile = serverBashFile;
+            this.serverBashArgs = serverBashArgs;
+            this.serverRunFilePath = serverRunFilePath;
+        }
+
+        public void ShutdownServerAsync()
+        {
+            var args = screenExecArgs.ToArray();
+            args[SCREEN_EXEC_ARG_COMMAND_POSITION] = "'quit'";
+
+            Cli.Wrap(SCREEN_CMD)
+                .WithArguments(args)
+                .ExecuteAsync();
+        }
+
+        public void ExecuteServerCommand(string command)
+        {
+            var args = screenExecArgs.ToArray();
+            args[SCREEN_EXEC_ARG_COMMAND_POSITION] = $"'{command}'";
+
+            Cli.Wrap(SCREEN_CMD)
+                .WithArguments(args)
+                .ExecuteAsync();
+        }
+
+        public string SeeConsoleLogs()
+        {
+            var filePath = this.serverRunFilePath.Last() == '/' ||
+                           this.serverRunFilePath.Last() == '\\' ?
+                            this.serverRunFilePath + "server-console.txt" : "/server-console.txt";
+
+            return File.ReadAllText(filePath);
+        }
+
+        public void StartServer()
+        {
+            Cli.Wrap(serverBashFile)
+                .WithArguments(serverBashArgs)
+                .WithWorkingDirectory(serverBashDirectory)
+                .ExecuteAsync();
+        }
+
+        private void InitScreen()
+        {
+            Cli.Wrap(SCREEN_CMD)
+                .WithArguments(new[] { "-dmS", SCREEN_SOCKNAME })
+                .ExecuteAsync().Task.Wait();
+        }
+    }
+}
